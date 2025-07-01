b5e8a6f01cbd996340decfc5387107cab46e62f4
Tick-git <62646477+Tick-git@users.noreply.github.com>
2025-07-01 16:26:00 +0200

    Convert RecipeApp to ASP.NET Core Web API
    Intention:
    - Convert the existing Entity Framework project into a minimal ASP.NET Core WebApp with a single GET endpoint to understand the initial steps of exposing data via a web API

--- RecipeApp/Program.cs
+++ RecipeApp/Program.cs
@@ -1,5 +1,4 @@
 ﻿using RecipeApp.Data;
-using RecipeApp.Dto;
 using RecipeApp.Service;
 
 namespace RecipeApp
@@ -13,43 +12,17 @@ namespace RecipeApp
                 string recipeName = "Cookies";
 
                 IRecipeService recipeService = new RecipeService(
-                    new RecipeRepository(context), 
+                    new RecipeRepository(context),
                     new IngredientRepository(context),
                     new QuantityUnitRepository(context));
 
-                RecipeDto? cookieRecipeDto = recipeService.GetRecipeByName(recipeName);
+                var builder = WebApplication.CreateBuilder(args);
+                var app = builder.Build();
 
-                if (cookieRecipeDto == null)
-                {
-                    cookieRecipeDto = GetCookieRecipeDto(recipeName);
+                app.MapGet("/", () => recipeService.GetRecipeByName(recipeName));
 
-                    recipeService.AddRecipe(cookieRecipeDto);
-
-                    Console.WriteLine("Cookie Recipe added");
-                }
-                else
-                {
-                    Console.WriteLine("Cookie Recipe needs: ");    
-                    
-                    foreach(var recipeIngredientDto in cookieRecipeDto.RecipeIngredientDtos)
-                    {
-                        Console.WriteLine($"    {recipeIngredientDto.Name}  {recipeIngredientDto.Quantity} {recipeIngredientDto.UnitSymbol}");
-                    }
-                }
+                app.Run();
             }
-        }
-
-        private static RecipeDto GetCookieRecipeDto(string recipeName)
-        {
-            List<RecipeIngredientDto> recipeIngredientDto = new()
-            {
-                new RecipeIngredientDto() { Name = "Sugar", Quantity = 100.0f, UnitSymbol = "g"},
-                new RecipeIngredientDto() { Name = "Flour", Quantity = 200.0f, UnitSymbol = "g"},
-                new RecipeIngredientDto() { Name = "Baking soda", Quantity = 1.0f, UnitSymbol = "tsp"},
-
-            };
-
-            return new RecipeDto() { Name = recipeName, RecipeIngredientDtos = recipeIngredientDto }; 
         }
     }
 }
